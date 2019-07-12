e0dd7e770c4a91b97b4959ae265a533e2b21231b
Lucy Suddenly <suddenlylucy@gmail.com>
2019-07-12 08:46:04 -0700

    basic search page structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Search from './components/search.js'
 
 function App() {
   return (
--- src/components/search.js
+++ src/components/search.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
 
 class Search extends Component {
     render() {
         return (
-            <div>
-                
+            <div id = "SearchDiv">
+                <Form>
+                    <FormControl name="search" type="text" placeholder="Search for companies to review"/>
+                    <Button type="submit">Search</Button>
+                </Form>
             </div>
         );
     }
