0f1e800dbf6e4996961c791ad734a625018ed219
Lucy Suddenly <suddenlylucy@gmail.com>
2019-07-12 10:09:42 -0700

    removed CSS, added state and fetch to search component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Search from './components/search.js'
+import Navbar from 'react-bootstrap/Navbar'
 
 function App() {
   return (
+    <>
+    <Navbar collapseOnSelect expand="md" sticky="top" bg="success" variant="dark">
+      <Navbar.Brand href="/">
+        Working While Trans
+      </Navbar.Brand>
+       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+       <Navbar.Collapse id="responsive-navbar-nav"> 
+       </Navbar.Collapse>
+    </Navbar>
     <Router>
       <Switch>
         <Route exact path="/" render={()=>(<Search/>)}/>
       </Switch>
     </Router>
+    </>
   );
 }
 
--- src/components/search.js
+++ src/components/search.js
@@ -4,11 +4,36 @@ import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
 
 class Search extends Component {
+    constructor(){
+        super()
+        this.state={
+            searchForm: ""
+        }
+    }
+
+    submitSearch = (ev) => {
+        ev.preventDefault()
+        fetch("http://localhost:3000/search", {
+          method: "POST",
+          headers:{
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: JSON.stringify(this.state)
+        })
+    }
+
+    searchFieldChange = (ev) => {
+        this.setState({
+            [ev.target.name]: [ev.target.value]
+        })
+    }
+
     render() {
         return (
             <div id = "SearchDiv">
-                <Form>
-                    <FormControl name="search" type="text" placeholder="Search for companies to review"/>
+                <Form onSubmit={(ev) => this.submitSearch(ev)}>
+                    <FormControl name="searchForm" type="text" placeholder="Search for companies to review" onChange={(ev) => this.searchFieldChange(ev)}/>
                     <Button type="submit">Search</Button>
                 </Form>
             </div>
