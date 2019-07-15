fbf5902e817b97c5413c53a32d6d7841bace6c84
Lucy Suddenly <suddenlylucy@gmail.com>
2019-07-15 12:00:22 -0700

    added new review form to company show

--- src/components/companyShow.js
+++ src/components/companyShow.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import {withRouter} from 'react-router';
 import Review from './review.js'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
 
 class CompanyShow extends Component {
     constructor(props){
@@ -12,7 +15,11 @@ class CompanyShow extends Component {
                 image_url: null,
                 description: null,
                 reviews: []
-            }
+            },
+            title: "",
+            job_title: "",
+            body: "",
+            rating: ""
         }
         this.fetchShowData()
     }
@@ -25,6 +32,12 @@ class CompanyShow extends Component {
         }))
     }
 
+    onTextFormChange = (ev) => {
+        this.setState({
+            [ev.target.name]: ev.target.value
+        })
+    }
+
     render() {
         return (
             <>
@@ -33,6 +46,19 @@ class CompanyShow extends Component {
                 <a href={this.state.company.website}>{this.state.company.website}</a>
                 <img src={this.state.company.image_url}/>
                 <p>{this.state.company.description}</p>
+            </div>
+            <div>
+            <Form onSubmit={(ev)=> this.submitForm(ev)}>
+                    <Form.Label>Title of Review</Form.Label>
+                    <FormControl value={this.state.title} onChange={(ev) => this.onTextFormChange(ev)} name="title" type="text" placeholder="Title of Review"/>
+                    <Form.Label>Your Job Title</Form.Label>
+                    <FormControl value={this.state.job_title} onChange={(ev) => this.onTextFormChange(ev)} name="job_title" type="text" placeholder="Your Job Title"/>
+                    <Form.Label>Review</Form.Label>
+                    <FormControl value={this.state.body} onChange={(ev) => this.onTextFormChange(ev)} name="body" as="textarea" placeholder="Your Review" rows="5"/>
+                    <Form.Label>Company Description</Form.Label>
+                    <FormControl value={this.state.rating} onChange={(ev) => this.onTextFormChange(ev)} name="rating" type="number" placeholder="Rating from 1-5"/>
+                    <Button variant="primary" type="submit">Submit New Review</Button>
+            </Form>
             </div>
             <div>
                 {this.state.company.reviews.map(review => {
