22cd50c8827c50fe1690f1191c25e20cc71cfe12
Lucy Suddenly <suddenlylucy@gmail.com>
2019-07-15 08:13:43 -0700

    added company show page structure"

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Search from './components/search.js'
 import NewCompany from './components/newCompany.js'
+import CompanyShow from './components/companyShow.js'
 import Navbar from 'react-bootstrap/Navbar'
+
 
 class App extends Component {
 
@@ -22,7 +24,8 @@ class App extends Component {
       <Router>
         <Switch>
           <Route exact path="/" render={()=>(<Search/>)}/>
-          <Route exact parth="/companies/new" render={() =>(<NewCompany/>)}/>
+          <Route exact path="/companies/new" render={() =>(<NewCompany/>)}/>
+          <Route exact path="/companies/:id" render={() => (<CompanyShow/>)}/>
         </Switch>
       </Router>
       </>
---
+++ src/components/companyShow.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {withRouter} from 'react-router';
+
+class CompanyShow extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default withRouter(CompanyShow);
